561d67b2aa73b6f81a6ed4c67bd5b3daa5806f88
lirc572 <lirc572@gmail.com>
2021-12-03 02:32:32 +0800

    initial

---
+++ main.ts
@@ -0,0 +1,32 @@
+import { serve  } from "https://deno.land/std@0.116.0/http/server.ts";
+
+serve(async (req) => {
+      const url = new URL(req.url);
+      url.protocol = "https:";
+      url.hostname = "google.com";
+      url.port = "443";
+      const resp = await fetch(url.href);
+
+      const bodyProcessed = resp.body
+        .pipeThrough(new TextDecoderStream())
+        .pipeThrough(
+            new TransformStream({
+                transform: (chunk, controller) => {
+                              controller.enqueue(chunk.replaceAll('google.com', 'glegoo.deno.dev'));
+                            
+                },
+                      
+            }),
+                
+        )
+        .pipeThrough(new TextEncoderStream());
+
+    return new Response(bodyUpperCase, {
+            status: resp.status,
+            headers: resp.headers,
+          
+    });
+    
+});
+
+console.log("Listening on http://localhost:8000");
